b8d054323367f7136d75f1b2bac2da19e7e900a2
mya todd <mya.todd428376@gmail.com>
2021-01-19 19:43:24 -0500

    added api routes

--- public/assets/js/sever.js
+++ public/assets/js/sever.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 app.get('/notes', function (req, res) {
-  res.send('GET request to the notes.html')})
+  res.send('GET return to the notes.html')})
+
+  app.get('/api/notes', function (req, res) {
+    res.send('GET return to the db.json')})
+
+    app.post('/api/notes', function (req, res) {
+        res.send('POST request and receive then add then return to the db.json')})
+
   app.get('/*', function (req, res) {
-    res.send('GET request to the index.html')})
+    res.send('GET return to the index.html')})
+
+    app.delete('/api/notes/:id', function (req, res) {
+        res.send('DELETE return to the db.json')})
+    
 
 
 
